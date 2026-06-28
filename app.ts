7af229d4fa44770be2e96687b17f2d1a23535817
cafe small house <ybaram40@gmail.com>
2026-06-28 15:29:48 +0900

    typescript 로직 추가

--- app.ts
+++ app.ts
@@ -6,27 +6,29 @@ const mainPath = '/Users/jeongbin/Downloads';
 const REGEX = Object.freeze({
   IMAGE_REGEX: /(\.jpg|\.jpeg|\.png|\.gif|\.svg|\.avif|\.webp)$/i,
   VIDEO_REGEX: /\.mp4$|\.avi$|\.mov$|\.wmv$|\.flv$/i,
+  AUDIO_REGEX: /\.mp3$/i,
   FILE_REGEX:
-    /\.pdf$|\.doc$|\.docx$|\.ppt$|\.pptx$|\.xls$|\.xlsx$|\.hwp$|\.txt$|\.zip$|\.7z$|\.rar$|\.tar$|\.gz$|\.xd$|\.iso|\.ips|\.ipa|\.psd|\.ai|\.xd|\.json/i,
+    /\.pdf$|\.doc$|\.docx$|\.ppt$|\.pptx$|\.xls$|\.xlsx$|\.csv$|\.hwp$|\.txt$|\.md$|\.html?$|\.zip$|\.7z$|\.rar$|\.tar$|\.gz$|\.xd$|\.iso|\.ips|\.ipa|\.psd|\.ai|\.xd|\.kra|\.json/i,
   PDF_REGEX: /\.pdf$/i,
-  XLS_REGEX: /\.xls$|\.xlsx$/i,
+  XLS_REGEX: /\.xls$|\.xlsx$|\.csv$/i,
   ZIP_REGEX: /\.zip$|\.7z$|\.rar$|\.tar$|\.gz$|\.iso$/i,
   HWP_REGEX: /\.hwp$/i,
+  MARKDOWN_REGEX: /\.md$/i,
+  HTML_REGEX: /\.html?$/i,
   ANDROID_REGEX: /\.apk|\.aab$/i,
   IOS_REGEX: /\.ips|\.ipa$/i,
-  PHOTO_SHOP_REGEX: /\.psd|\.ai|\.xd$/i,
+  PHOTO_SHOP_REGEX: /\.psd|\.ai|\.xd$|\.kra$/i,
   JSON_REGEX: /\.json$/i,
 });
 
 /**
  * * 파일 복사
  * @description 파일 복사 후 기존 파일 삭제
- * @param {string} path - 파일이 있는 경로
- * @param {string} datePath - 날짜별 폴더 경로
- * @param {string} item - 파일 이름
- * @returns {void}
+ * @param path - 파일이 있는 경로
+ * @param datePath - 날짜별 폴더 경로
+ * @param item - 파일 이름
  */
-const onCopyFileHandler = (path, datePath, item) => {
+const onCopyFileHandler = (path: string, datePath: string, item: string): void => {
   fs.copyFile(`${path}/${item}`, `${datePath}/${item}`, (err) => {
     if (err) throw err;
 
@@ -43,10 +45,9 @@ const onCopyFileHandler = (path, datePath, item) => {
 /**
  * * 폴더 생성
  * @description 폴더가 없을 경우 폴더 생성
- * @param {string} path - 폴더 경로
- * @returns {void}
+ * @param path - 폴더 경로
  */
-const onMkdirHandler = (path) => {
+const onMkdirHandler = (path: string): void => {
   if (!fs.existsSync(path)) {
     fs.mkdirSync(path);
   }
@@ -55,20 +56,24 @@ const onMkdirHandler = (path) => {
 /**
  * * 파일 정리
  * @description 파일 정리을 정리해주는 main 함수입니다.
- * @param {string} path
+ * @param path
  */
-const onFileCleaner = (path) => {
+const onFileCleaner = (path: string): void => {
   const imagePath = path + '/_day_images';
   const videoPath = path + '/_day_videos';
+  const audioPath = path + '/_day_audios';
   const filePath = path + '/_day_files';
   const date = dayjs().format('YYYY-MM-DD');
   const imageDateFolder = `${imagePath}/${date}`;
   const videoDateFolder = `${videoPath}/${date}`;
+  const audioDateFolder = `${audioPath}/${date}`;
   const fileDateFolder = `${filePath}/${date}`;
   const fileDatePdf = `${fileDateFolder}/pdf`;
   const fileDateXls = `${fileDateFolder}/xls`;
   const fileDateZip = `${fileDateFolder}/zip`;
   const fileDateHwp = `${fileDateFolder}/hwp`;
+  const fileDateMarkdown = `${fileDateFolder}/markdown`;
+  const fileDateHtml = `${fileDateFolder}/html`;
   const fileDateAndroid = `${fileDateFolder}/android`;
   const fileDateIos = `${fileDateFolder}/ios`;
   const fileDatePhotoShop = `${fileDateFolder}/photo-shop`;
@@ -77,14 +82,18 @@ const onFileCleaner = (path) => {
 
   onMkdirHandler(imagePath);
   onMkdirHandler(videoPath);
+  onMkdirHandler(audioPath);
   onMkdirHandler(filePath);
   onMkdirHandler(imageDateFolder);
   onMkdirHandler(videoDateFolder);
+  onMkdirHandler(audioDateFolder);
   onMkdirHandler(fileDateFolder);
   onMkdirHandler(fileDatePdf);
   onMkdirHandler(fileDateXls);
   onMkdirHandler(fileDateZip);
   onMkdirHandler(fileDateHwp);
+  onMkdirHandler(fileDateMarkdown);
+  onMkdirHandler(fileDateHtml);
   onMkdirHandler(fileDateAndroid);
   onMkdirHandler(fileDateIos);
   onMkdirHandler(fileDatePhotoShop);
@@ -98,11 +107,14 @@ const onFileCleaner = (path) => {
       const {
         IMAGE_REGEX,
         VIDEO_REGEX,
+        AUDIO_REGEX,
         FILE_REGEX,
         PDF_REGEX,
         XLS_REGEX,
         ZIP_REGEX,
         HWP_REGEX,
+        MARKDOWN_REGEX,
+        HTML_REGEX,
         ANDROID_REGEX,
         IOS_REGEX,
         PHOTO_SHOP_REGEX,
@@ -122,7 +134,12 @@ const onFileCleaner = (path) => {
           return;
         }
 
-        if (IMAGE_REGEX.test(item) || VIDEO_REGEX.test(item) || FILE_REGEX.test(item)) {
+        if (
+          IMAGE_REGEX.test(item) ||
+          VIDEO_REGEX.test(item) ||
+          AUDIO_REGEX.test(item) ||
+          FILE_REGEX.test(item)
+        ) {
           if (IMAGE_REGEX.test(item)) {
             onCopyFileHandler(path, imageDateFolder, item);
           }
@@ -131,11 +148,17 @@ const onFileCleaner = (path) => {
             onCopyFileHandler(path, videoDateFolder, item);
           }
 
+          if (AUDIO_REGEX.test(item)) {
+            onCopyFileHandler(path, audioDateFolder, item);
+          }
+
           if (FILE_REGEX.test(item)) {
             if (PDF_REGEX.test(item)) onCopyFileHandler(path, fileDatePdf, item);
             if (XLS_REGEX.test(item)) onCopyFileHandler(path, fileDateXls, item);
             if (ZIP_REGEX.test(item)) onCopyFileHandler(path, fileDateZip, item);
             if (HWP_REGEX.test(item)) onCopyFileHandler(path, fileDateHwp, item);
+            if (MARKDOWN_REGEX.test(item)) onCopyFileHandler(path, fileDateMarkdown, item);
+            if (HTML_REGEX.test(item)) onCopyFileHandler(path, fileDateHtml, item);
             if (ANDROID_REGEX.test(item)) onCopyFileHandler(path, fileDateAndroid, item);
             if (IOS_REGEX.test(item)) onCopyFileHandler(path, fileDateIos, item);
             if (PHOTO_SHOP_REGEX.test(item)) onCopyFileHandler(path, fileDatePhotoShop, item);
@@ -145,6 +168,8 @@ const onFileCleaner = (path) => {
               !XLS_REGEX.test(item) &&
               !ZIP_REGEX.test(item) &&
               !HWP_REGEX.test(item) &&
+              !MARKDOWN_REGEX.test(item) &&
+              !HTML_REGEX.test(item) &&
               !ANDROID_REGEX.test(item) &&
               !IOS_REGEX.test(item) &&
               !PHOTO_SHOP_REGEX.test(item) &&
